1ecf8fa8f98ef544aa2daedd5a993498ab8972dd
taobupt <wangtaoo665@gmail.com>
2016-12-31 15:08:57 -0600

    add junit test

--- src/Fundamental/BinarySearch.java
+++
@@ -1,8 +0,0 @@
-package Fundamental;
-
-/**
- * Created by Tao on 12/31/2016.
- */
-public class BinarySearch {
-
-}
---
+++ src/test/java/Fundamental/BinarySearchTest.java
@@ -0,0 +1,26 @@
+package Fundamental;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tao on 12/31/2016.
+ */
+public class BinarySearchTest {
+
+    public BinarySearch binarySearch=null;
+    @Before
+    public void setUp() throws Exception {
+        binarySearch=new BinarySearch();
+    }
+
+    @Test
+    public void testIndexOf() throws Exception {
+
+        int []nums={1,2,3,4,5,6};
+        int index=binarySearch.indexOf(nums,4);
+        assertEquals(index,3);
+    }
+}
