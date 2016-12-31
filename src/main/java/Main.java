942287438afb454748d8c4031d2fa7ae8cc06e24
taobupt <wangtaoo665@gmail.com>
2016-12-31 17:30:29 -0600

    add javaio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 
 
 
+import Fundamental.Average;
 import Fundamental.BinarySearch;
+import Fundamental.Concate;
 import org.apache.commons.lang3.*;
 import java.util.*;
 
@@ -10,19 +12,11 @@ import java.util.*;
  */
 public class Main {
     public static void main(String []args){
-        int []nums={-6,-5,-4,-3,-1,4,5,8};
-        Arrays.sort(nums);
-        Integer []num=ArrayUtils.toObject(nums);
-        Arrays.sort(num, new Comparator<Integer>() {
-            public int compare(Integer o1, Integer o2) {
-                return -o1.compareTo(o2);
-            }
-        });
-        for(Integer x:num)
-            System.out.println(x);
-
-
-        BinarySearch s=new BinarySearch();
-        System.out.println(s.indexOf(nums,-3));
+        Concate conca=new Concate();
+        String[]filenames={"A:\\log1.txt","A:\\log2.txt","A:\\log3.txt"};
+        String des="A:\\log.txt";
+        conca.concate(filenames,des);
+        System.out.println(System.getProperty("line.separator"));
     }
+
 }
